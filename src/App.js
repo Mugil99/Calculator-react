04628d60b13d3805fbea962fd65e726f180e775d
Mugil99 <mugilya99@gmail.com>
2023-05-15 21:04:20 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+let arr1=["C","/","*","-"]
+let arr2=["7","8","9","+"]
+let arr3=["4","5","6"]
+let arr4=["1","2","3","="]
+let arr5=["0",".",""]
 
 function App() {
+
+  let [str,setStr]=useState("");
+  function getinput(e){
+    setStr(str+e.target.innerText);
+    console.log(str);
+  }
+  function Clear(){
+    setStr("");
+  }
+  function Calculate(){
+    try{
+      let ans=eval(str);
+      setStr(ans);
+    }catch(err){
+      setStr("Error");
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+    <input type="text" value={str}></input>
+    <table>
+      <tr>
+        {arr1.map((item)=>{
+          return <td onClick={item=="C"?Clear:getinput}>{item}</td>
+        })}
+      </tr>
+      <tr>
+        {arr2.map((item)=>{
+          return <td onClick={getinput} rowSpan={item=="+"&& 2}>{item}</td>
+        })}
+      </tr>
+      <tr>
+        {arr3.map((item)=>{
+            return <td onClick={getinput}>{item}</td>
+        })}
+      </tr>
+      <tr>
+        {arr4.map((item)=>{
+          return <td onClick={item=="="?Calculate:getinput} rowSpan={item=="="&& 2}>{item}</td>
+        })}
+      </tr>
+      <tr>
+        {arr5.map((item)=>{
+          return <td onClick={getinput}>{item}</td>
+        })}
+      </tr>
+    </table>
     </div>
   );
 }
